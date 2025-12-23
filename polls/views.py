d57262e05961c3003eaab7247d6d0e395a7e3b23
Rob Fowler <robtf04@proton.me>
2025-12-23 07:44:06 -0500

    added admin page and basic views for polls app

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+# polls/views.py
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +7,14 @@ def index(request):
     return HttpResponse("This is the polls app index.")
 
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
