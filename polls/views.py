c636112229d2f027099c6de6b7cca956d9e538d7
Rob Fowler <robtf04@proton.me>
2025-12-23 08:02:52 -0500

    added updated view and index template to polls app

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,15 @@
 # polls/views.py
 
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 def index(request):
-    return HttpResponse("This is the polls app index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {"latest_question_list":latest_question_list}
+    return render(request, "polls/index.html",context)
+
+                        
 
 
 def detail(request, question_id):
@@ -18,3 +23,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
